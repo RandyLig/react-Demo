bae91106b59c8fb16b18c4c04491ce77541ac3a4
lg <1094449267@qq.com>
2017-09-20 23:34:39 +0800

    first

--- src/components/guide/guide.jsx
+++ src/components/guide/guide.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { render } from "react-dom";
 import GuideHeader from './guide-header'
 import SchoolAround from './school-around'
+import MenuLeft from '../menu/menu'
 export default class Guide extends React.Component {
     render() {
         return <div>
             <GuideHeader></GuideHeader>
             <SchoolAround></SchoolAround>
+            <MenuLeft></MenuLeft>
         </div>;
     }
 }
