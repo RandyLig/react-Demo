0e55ee3f2b096955aa9fccafec1550277b0c92aa
gang li <1094449267@qq.com>
2017-09-22 17:14:01 +0800

    logo

---
+++ src/components/login/login.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { render } from "react-dom";
+import LogoGather from './logo';
+import { Input, Icon } from 'antd';
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+        };
+        this.emitEmpty = () => {
+            this.userNameInput.focus();
+            this.setState({ userName: '' });
+        }
+        this.onChangeUserName = (e) => {
+            this.setState({ userName: e.target.value });
+        }
+    }
+
+    render() {
+        const { userName } = this.state;
+        const suffix = userName ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null;
+        return <div>
+            <LogoGather>
+
+            </LogoGather>
+            <Input
+                placeholder="Enter your userName"
+                prefix={<Icon type="user" />}
+                suffix={suffix}
+                value={userName}
+                onChange={this.onChangeUserName}
+                ref={node => this.userNameInput = node}
+                />
+        </div>;
+    }
+}
