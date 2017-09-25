5401aa66d7bddad8b997fcc9a667b1a21c0664c6
gang li <1094449267@qq.com>
2017-09-25 13:53:17 +0800

    add login

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { render } from "react-dom";
 import LogoGather from './logo';
-import { Input, Icon } from 'antd';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import MyNormalLoginForm from './form'
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
@@ -11,6 +12,11 @@ export default class Login extends React.Component {
         return <div>
             <LogoGather>
             </LogoGather>
+            <MyNormalLoginForm>
+            </MyNormalLoginForm>
         </div>;
     }
 }
+
+
+
